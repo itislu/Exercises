3b593756f932f06d0cbb53066f51bf030ecdbdb0
Lukas Dullinger <129603980+itislu@users.noreply.github.com>
2024-10-20 20:59:39 +0200

    [snail] feat: Implement Snail Sort

---
+++ codewars/python/snail/snail.py
@@ -0,0 +1,44 @@
+# First move is measurement of length of square
+# Then, every 2 moves length is decreased by 1
+def snail(snail_map):
+    res = []
+    lim = len(snail_map[0])
+    x, y = -1, 0
+    dir = 1
+
+    for _ in range(lim):
+        x += dir
+        res.append(snail_map[y][x])
+    lim -= 1
+
+    while lim > 0:
+        for _ in range(lim):
+            y += dir
+            res.append(snail_map[y][x])
+        dir *= -1
+        for _ in range(lim):
+            x += dir
+            res.append(snail_map[y][x])
+        lim -= 1
+
+    return res
+
+
+tests = (
+    [[1,2,3],
+     [4,5,6],
+     [7,8,9]],
+    [[1,2,3],
+     [8,9,4],
+     [7,6,5]]
+)
+expected = (
+    [1,2,3,6,9,8,7,4,5],
+    [1,2,3,4,5,6,7,8,9]
+)
+
+for test, expect in zip(tests, expected):
+    res = snail(test)
+    print("result:", res)
+    print("expect:", expect)
+    print()
